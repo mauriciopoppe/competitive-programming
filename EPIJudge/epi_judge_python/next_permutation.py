338007abcb754a7495cc20492d1f85c4f1079016
Mauricio Poppe <mauricio.poppe@gmail.com>
2020-06-02 12:15:58 -0700

    next permutation

--- EPIJudge/epi_judge_python/next_permutation.py
+++ EPIJudge/epi_judge_python/next_permutation.py
@@ -4,9 +4,20 @@ from test_framework import generic_test
 
 
 def next_permutation(perm: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    j = len(perm) - 1
+    while j - 1 >= 0 and perm[j - 1] >= perm[j]:
+        j -= 1
 
+    if j == 0:
+        return []
+
+    low = len(perm) - 1
+    while perm[low] <= perm[j - 1]:
+        low -= 1
+
+    perm[j - 1], perm[low] = perm[low], perm[j - 1]
+    perm[j:] = sorted(perm[j:])
+    return perm
 
 if __name__ == '__main__':
     exit(
