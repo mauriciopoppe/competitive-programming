555ad4a3465ea448936c7e3ce3b9fb57fe4becd3
Mauricio Poppe <mauricio.poppe@gmail.com>
2020-10-06 22:45:43 -0700

    comments for the threadpool

--- _learn/multithreading/src/ThreadPool.cpp
+++ _learn/multithreading/src/ThreadPool.cpp
@@ -2,11 +2,21 @@
 #include <queue>
 #include <vector>
 #include <thread>
+#include <functional>
+#include <mutex>
+#include <condition_variable>
 
 class ThreadPool {
+    // the jobs to be executed in the threadPool
     std::queue<std::function<void()>> q;
+
+    // the threads created in the threadPool
     std::vector<std::thread> threads;
+
+    // the mutex that controls access to the jobs queue
     std::mutex _m;
+
+    // the condition variable that pauses a thread if there are no jobs pending
     std::condition_variable _cond;
 
     void ThreadRunner() {
@@ -25,6 +35,8 @@ class ThreadPool {
                 q.pop();
             }
 
+            // guaranteed to exist, it can't be a reference to something invalid
+            // because the block above will pull an item from the queue if it exists
             fn();
         }
     }
@@ -33,12 +45,14 @@ public:
     ThreadPool(int n) {
         threads.resize(n);
         for (int i = 0; i < (int) threads.size(); i += 1) {
+            // https://www.justsoftwaresolutions.co.uk/threading/multithreading-in-c++0x-part-3.html
             std::thread t(&ThreadPool::ThreadRunner, std::ref(*this));
             threads[i] = std::move(t);
         }
     }
 
     void Enqueue(std::function<void()> fn) {
+        // any modification to the queue is controlled by the mutex
         std::unique_lock<std::mutex> g(_m);
         q.push(std::move(fn));
         _cond.notify_one();
